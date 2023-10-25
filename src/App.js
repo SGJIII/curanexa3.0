8f7d81d728bf6a3b861a9f809245bd94e677fa52
Sam Johnson <samueljoh@gmail.com>
2023-10-25 10:49:19 -0400

    added route dom for nav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
-import AnalyzePage from "./components/AnalyzePage";
-import AnalysisPage from "./components/AnalysisPage";
-import ImagesPage from "./components/ImagesPage";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Analyze from "./components/AnalyzePage";
+import Analysis from "./components/AnalysisPage";
+import Images from "./components/ImagesPage";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AnalyzePage />
-        <AnalysisPage />
-        <ImagesPage />
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Analyze</Link> | <Link to="/Images">Images</Link> |{" "}
+          <Link to="/Analysis">Analysis</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Analyze />} />
+          <Route path="/Analysis" element={<Analysis />} />
+          <Route path="/Images" element={<Images />} />
+        </Routes>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
