c3394acc81b14cd97c0abb6729da0f2496c10126
Sam Johnson <samueljoh@gmail.com>
2023-10-25 22:50:01 -0400

    linked frontend with backend on local server and added all css elements for frontend

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Analyze from "./components/AnalyzePage";
-import Analysis from "./components/AnalysisPage";
-import Images from "./components/ImagesPage";
+import AnalyzePage from "./components/AnalyzePage";
+import ImagesPage from "./components/ImagesPage";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <Link to="/">Analyze</Link> | <Link to="/Images">Images</Link> |{" "}
-          <Link to="/Analysis">Analysis</Link>
-        </nav>
+        <header>
+          <a
+            href="/"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.reload();
+            }}
+          >
+            <img src="/logo.png" alt="CuraNexa Logo" className="logo" />
+          </a>
+        </header>
         <Routes>
-          <Route path="/" element={<Analyze />} />
-          <Route path="/Analysis" element={<Analysis />} />
-          <Route path="/Images" element={<Images />} />
+          <Route path="/" element={<AnalyzePage />} />
+          <Route path="/Images" element={<ImagesPage />} />
         </Routes>
       </div>
     </Router>
--- src/components/AnalyzePage.js
+++ src/components/AnalyzePage.js
@@ -1,425 +1,112 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { useDropzone } from "react-dropzone";
+import "./AnalyzePage.css";
 
-class AnalyzePage extends Component {
-  state = {
-    selectedFile: null,
-    analysisResult: null,
-  };
+const AnalyzePage = () => {
+  const [files, setFiles] = useState([]);
+  const [result, setResult] = useState(null);
 
-  onFileChange = (event) => {
-    this.setState({ selectedFile: event.target.files[0] });
-  };
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
 
-  onFormSubmit = (event) => {
-    event.preventDefault();
-
+  const handleSubmit = () => {
     const formData = new FormData();
-    formData.append("image", this.state.selectedFile);
+    formData.append("image", files[0]);
 
     axios
       .post("http://localhost:3000/analyze", formData)
       .then((response) => {
-        console.log(response.data);
-        this.setState({ analysisResult: response.data.results });
-        // Redirect to AnalysisPage with the analysis results
-        this.props.history.push("/Analysis", {
-          analysisResult: this.state.analysisResult,
-        });
+        const analysisId = response.data.analysisId;
+        axios
+          .get(`http://localhost:3000/analysis/${analysisId}`)
+          .then((resultResponse) => {
+            setResult(resultResponse.data); // This assumes the returned data is your desired result.
+          })
+          .catch((error) => {
+            console.error("Error fetching the analysis results:", error);
+          });
       })
       .catch((error) => {
-        console.log(error);
+        console.error("Error during image upload:", error);
       });
   };
-  render() {
-    return (
-      <div>
-        {
-          <div
-            style={{
-              width: "100%",
-              height: "100%",
-              position: "relative",
-              background: "white",
-            }}
-          >
-            <div
-              style={{
-                width: 793,
-                height: 362,
-                left: 629,
-                top: 176,
-                position: "absolute",
-                background: "#C2EAEF",
-                border: "1px #10A3A7 dotted",
-              }}
-            />
-            <div
-              style={{
-                width: 280,
-                height: 853,
-                left: 0,
-                top: 129,
-                position: "absolute",
-                background: "white",
-              }}
-            />
-            <div
-              style={{
-                width: 235,
-                height: 514,
-                left: 319,
-                top: 176,
-                position: "absolute",
-              }}
-            >
-              <span
-                style={{
-                  color: "black",
-                  fontSize: 36,
-                  fontFamily: "Inter",
-                  fontWeight: "400",
-                  wordWrap: "break-word",
-                }}
-              >
-                How it works. <br />
-              </span>
-              <span
-                style={{
-                  color: "black",
-                  fontSize: 12,
-                  fontFamily: "Inter",
-                  fontWeight: "400",
-                  wordWrap: "break-word",
-                }}
-              >
-                <br />
-              </span>
-              <span
-                style={{
-                  color: "black",
-                  fontSize: 24,
-                  fontFamily: "Roboto Flex",
-                  fontWeight: "400",
-                  wordWrap: "break-word",
-                }}
-              >
-                Take a picture or obtain a digital copy of your lung xray.
-                <br />
-                Upload the image by dragging and dropping or searching for a
-                file.
-                <br />
-                Click Analyze to see your results
-              </span>
-            </div>
-            <div
-              style={{
-                width: 90,
-                height: 90,
-                left: 981,
-                top: 220,
-                position: "absolute",
-              }}
-            >
-              <div
-                style={{
-                  width: 84.01,
-                  height: 84,
-                  left: 3,
-                  top: 6,
-                  position: "absolute",
-                  background: "#848484",
-                }}
-              ></div>
-            </div>
-            <div
-              style={{
-                width: 486,
-                height: 37,
-                left: 783,
-                top: 331,
-                position: "absolute",
-                textAlign: "center",
-                color: "black",
-                fontSize: 24,
-                fontFamily: "Inter",
-                fontWeight: "400",
-                wordWrap: "break-word",
-              }}
-            >
-              Drag and Drop Image Here
-            </div>
-            <div
-              style={{
-                width: 486,
-                height: 37,
-                left: 783,
-                top: 376,
-                position: "absolute",
-                textAlign: "center",
-                color: "black",
-                fontSize: 24,
-                fontFamily: "Inter",
-                fontWeight: "400",
-                wordWrap: "break-word",
-              }}
-            >
-              or
-            </div>
-            <div
-              style={{
-                width: 169,
-                height: 43,
-                left: 937,
-                top: 452,
-                position: "absolute",
-                border: "2px #10A3A7 solid",
-              }}
-            />
-            <div
-              style={{
-                width: 27,
-                height: 27,
-                left: 1362,
-                top: 32,
-                position: "absolute",
-                background: "#D9D9D9",
-                borderRadius: 9999,
-              }}
-            />
-            <div
-              style={{
-                width: 75,
-                height: 22,
-                left: 1400,
-                top: 37,
-                position: "absolute",
-                color: "black",
-                fontSize: 12,
-                fontFamily: "Inter",
-                fontWeight: "400",
-                wordWrap: "break-word",
-              }}
-            >
-              User
-            </div>
-            <div
-              style={{
-                width: 56,
-                height: 56,
-                left: 47,
-                top: 30,
-                position: "absolute",
-              }}
-            >
-              <div
-                style={{
-                  width: 56,
-                  height: 56,
-                  left: 0,
-                  top: 0,
-                  position: "absolute",
-                  background: "#D9D9D9",
-                  borderRadius: 9999,
-                }}
-              />
-              <img
-                style={{
-                  width: 170.16,
-                  height: 170.16,
-                  left: -57,
-                  top: -44.7,
-                  position: "absolute",
-                }}
-                src="https://via.placeholder.com/170x170"
-                alt="profile"
-              />
-            </div>
-            <div
-              style={{
-                width: 200,
-                height: 53,
-                left: 119,
-                top: 33,
-                position: "absolute",
-                color: "black",
-                fontSize: 30,
-                fontFamily: "Inter",
-                fontWeight: "700",
-                wordWrap: "break-word",
-              }}
-            >
-              CuraNexa
-            </div>
-            <div
-              style={{
-                width: 106,
-                height: 22,
-                left: 970,
-                top: 463,
-                position: "absolute",
-                color: "#10A3A7",
-                fontSize: 19,
-                fontFamily: "Roboto Flex",
-                fontWeight: "400",
-                wordWrap: "break-word",
-              }}
-            >
-              Browse Files
-            </div>
-            <div
-              style={{
-                width: 1512,
-                height: 0,
-                left: 0,
-                top: 109,
-                position: "absolute",
-                border: "1px #DDDDDD solid",
-              }}
-            ></div>
-            <div
-              style={{
-                width: 873,
-                height: 0,
-                left: 289,
-                top: 109,
-                position: "absolute",
-                transform: "rotate(90deg)",
-                transformOrigin: "0 0",
-                border: "1px #DDDDDD solid",
-              }}
-            ></div>
-            <div
-              style={{
-                width: 150,
-                height: 26,
-                left: 336,
-                top: 50,
-                position: "absolute",
-              }}
-            >
-              <span
-                style={{
-                  color: "#BABABA",
-                  fontSize: 12,
-                  fontFamily: "Roboto Flex",
-                  fontWeight: "700",
-                  wordWrap: "break-word",
-                }}
-              >
-                User &gt;{" "}
-              </span>
-              <span
-                style={{
-                  color: "#908C8C",
-                  fontSize: 12,
-                  fontFamily: "Roboto Flex",
-                  fontWeight: "700",
-                  wordWrap: "break-word",
-                }}
-              >
-                Analyze
-              </span>
-            </div>
-            <div
-              style={{
-                width: 248,
-                height: 49,
-                left: 912,
-                top: 600,
-                position: "absolute",
-                background: "#10A3A7",
-                borderRadius: 32,
-              }}
-            />
-            <div
-              style={{
-                width: 112,
-                height: 20,
-                left: 982,
-                top: 615,
-                position: "absolute",
-                textAlign: "center",
-                color: "white",
-                fontSize: 21,
-                fontFamily: "Roboto Flex",
-                fontWeight: "700",
-                wordWrap: "break-word",
-              }}
-            >
-              Analyze
-            </div>
-            <div
-              style={{
-                width: 105,
-                height: 21,
-                left: 93,
-                top: 208,
-                position: "absolute",
-                color: "black",
-                fontSize: 15,
-                fontFamily: "Roboto Flex",
-                fontWeight: "700",
-                wordWrap: "break-word",
-              }}
-            >
-              Analyze
-            </div>
-            <div
-              style={{
-                width: 105,
-                height: 21,
-                left: 93,
-                top: 254,
-                position: "absolute",
-                color: "black",
-                fontSize: 15,
-                fontFamily: "Roboto Flex",
-                fontWeight: "700",
-                wordWrap: "break-word",
-              }}
-            >
-              Images
-            </div>
-            <div
-              style={{
-                width: 27,
-                height: 20,
-                left: 49,
-                top: 252,
-                position: "absolute",
-                background: "#10A3A7",
-              }}
-            ></div>
-            <div
-              style={{
-                width: 28,
-                height: 28,
-                left: 49,
-                top: 204,
-                position: "absolute",
-              }}
-            >
-              <div
-                style={{
-                  width: 28,
-                  height: 28,
-                  left: 0,
-                  top: 0,
-                  position: "absolute",
-                  background: "#10A3A7",
-                }}
-              ></div>
-            </div>
-          </div>
-        }
-        <form onSubmit={this.onFormSubmit}>
-          <input type="file" onChange={this.onFileChange} />
-          <button type="submit">Analyze</button>
-        </form>
+
+  console.log(result);
+
+  return (
+    <div className="container">
+      <div className="how-it-works">
+        <h2>How it works</h2>
+        <ol className="no-numbers">
+          <li>Take a picture or obtain a digital copy of your lung xray.</li>
+          <li>
+            Upload the image by dragging and dropping or searching for a file.
+          </li>
+          <li>Click Analyze to see your results.</li>
+        </ol>
       </div>
-    );
-  }
-}
+      <div
+        {...getRootProps({
+          className: `dropzone ${files.length > 0 ? "no-border" : ""}`,
+        })}
+      >
+        <input {...getInputProps()} />
+        {files.length === 0 ? (
+          <>
+            <p>Drag and Drop Image Here</p>
+            <p>or</p>
+            <button className="browse-btn">Browse Files</button>
+          </>
+        ) : (
+          <img
+            src={files[0].preview}
+            alt={files[0].name}
+            style={{ width: "500px", height: "auto" }} // adjust the size as per your requirement
+          />
+        )}
+      </div>
+      {!result && (
+        <button className="analyze-btn" onClick={handleSubmit}>
+          Analyze
+        </button>
+      )}
+
+      {/* Display the result */}
+      {result && result.preds && (
+        <div className="analysis-section">
+          <h3 className="results-label">Analysis Results</h3>
+          <div className="results-container">
+            {Object.entries(result.preds)
+              .sort(([, valueA], [, valueB]) => valueB - valueA) // Sort in descending order
+              .map(([key, value]) => (
+                <div className="result-item" key={key}>
+                  <p className="condition-and-percentage">
+                    {key}: {(value * 100).toFixed(2)}%
+                  </p>
+                  <div className="percentage-bar">
+                    <div
+                      className="filled-bar"
+                      style={{ width: `${value * 100}%` }}
+                    ></div>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default AnalyzePage;
