4d74b6bc9e3cfec581c1d13b8821f14b875247fe
Sam Johnson <samueljoh@gmail.com>
2023-11-07 23:03:06 -0500

    added context based on model thresholds

--- src/components/AnalyzePage.js
+++ src/components/AnalyzePage.js
@@ -11,6 +11,71 @@ const AnalyzePage = () => {
   const [files, setFiles] = useState([]);
   const [result, setResult] = useState(null);
 
+  const op_threshs = {
+    Atelectasis: 0.07422872,
+    Consolidation: 0.038290843,
+    Infiltration: 0.09814756,
+    Pneumothorax: 0.0098118475,
+    Edema: 0.023601074,
+    Emphysema: 0.0022490358,
+    Fibrosis: 0.010060724,
+    Effusion: 0.103246614,
+    Pneumonia: 0.056810737,
+    Pleural_Thickening: 0.026791653,
+    Cardiomegaly: 0.050318155,
+    Nodule: 0.023985857,
+    Mass: 0.01939503,
+    Hernia: 0.042889766,
+    "Lung Lesion": 0.053369623,
+    Fracture: 0.035975814,
+    "Lung Opacity": 0.20204692,
+    "Enlarged Cardiomediastinum": 0.05015312,
+  };
+
+  const ppv80_thres = {
+    Atelectasis: 0.72715247,
+    Consolidation: 0.8885005,
+    Infiltration: 0.92493945,
+    Pneumothorax: 0.6527224,
+    Edema: 0.68707734,
+    Emphysema: 0.7304924,
+    Fibrosis: 0.7272054,
+    Effusion: 0.6127343,
+    Pneumonia: 0.9878492,
+    Pleural_Thickening: 0.61979693,
+    Cardiomegaly: 0.66309816,
+    Nodule: 0.7853459,
+    Mass: 0.930661,
+    Hernia: 0.93645346,
+    "Lung Lesion": 0.6788558,
+    Fracture: 0.6547198,
+    "Lung Opacity": 0.61614525,
+    "Enlarged Cardiomediastinum": 0.8489876,
+  };
+
+  const categorizePathologies = (results) => {
+    let highlyLikely = [];
+    let possible = [];
+    let likelyRuledOut = [];
+
+    Object.entries(results).forEach(([condition, confidence]) => {
+      if (confidence >= ppv80_thres[condition]) {
+        highlyLikely.push({ condition, confidence });
+      } else if (confidence >= op_threshs[condition]) {
+        possible.push({ condition, confidence });
+      } else {
+        likelyRuledOut.push({ condition, confidence });
+      }
+    });
+
+    // Sort each category by confidence in descending order
+    highlyLikely = highlyLikely.sort((a, b) => b.confidence - a.confidence);
+    possible = possible.sort((a, b) => b.confidence - a.confidence);
+    likelyRuledOut = likelyRuledOut.sort((a, b) => b.confidence - a.confidence);
+
+    return { highlyLikely, possible, likelyRuledOut };
+  };
+
   useEffect(() => {
     console.log("Result object:", result); // Log the result state to see if it is updated correctly
 
@@ -103,21 +168,119 @@ const AnalyzePage = () => {
           Analyze
         </button>
       )}
-
       {loading && ( // Render the loading spinner when loading is true
         <div className="loading-container">
           <img src="/spinner.gif" alt="Loading..." />
         </div>
       )}
-
       {/* Display the result */}
+      {result &&
+        (() => {
+          const { highlyLikely, possible, likelyRuledOut } =
+            categorizePathologies(result);
 
+          return (
+            <div className="analysis-section full-width">
+              {/* Section 1: Highly Likely */}
+              <section className="results-section">
+                <h4 className="categorie-title">Highly Likely:</h4>
+                {highlyLikely.length > 0 ? (
+                  <>
+                    <p className="categorie-description">
+                      These pathologies have at least an 80% likelihood of being
+                      present.
+                    </p>
+                    <div className="results-grid">
+                      {highlyLikely.map(({ condition, confidence }) => (
+                        <div className="result-item" key={condition}>
+                          <p className="condition-and-percentage">
+                            {condition}: {(confidence * 100).toFixed(2)}%
+                          </p>
+                          <div className="percentage-bar">
+                            <div
+                              className="filled-bar"
+                              style={{ width: `${confidence * 100}%` }}
+                            ></div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                ) : (
+                  <p className="categorie-description">
+                    There are no pathologies with a high likelihood of being
+                    present.
+                  </p>
+                )}
+              </section>
+
+              {/* Section 2: Possible */}
+              {possible.length > 0 && (
+                <section className="results-section">
+                  <h4 className="categorie-title">More testing needed:</h4>
+                  <p className="categorie-description">
+                    These pathologies could not be ruled out due to the high
+                    sensitivity of this model.
+                  </p>
+                  <div className="results-grid">
+                    {possible.map(({ condition, confidence }) => (
+                      <div className="result-item" key={condition}>
+                        <p className="condition-and-percentage">
+                          {condition}: {(confidence * 100).toFixed(2)}%
+                        </p>
+                        <div className="percentage-bar">
+                          <div
+                            className="filled-bar"
+                            style={{ width: `${confidence * 100}%` }}
+                          ></div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </section>
+              )}
+
+              {/* Section 3: Likely Ruled Out */}
+              <section className="results-section">
+                <h4 className="categorie-title">Likely Ruled Out:</h4>
+                {likelyRuledOut.length > 0 ? (
+                  <>
+                    <p className="categorie-description">
+                      These pathologies are likely ruled out.
+                    </p>
+                    <div className="results-grid">
+                      {likelyRuledOut.map(({ condition, confidence }) => (
+                        <div className="result-item" key={condition}>
+                          <p className="condition-and-percentage">
+                            {condition}: {(confidence * 100).toFixed(2)}%
+                          </p>
+                          <div className="percentage-bar">
+                            <div
+                              className="filled-bar"
+                              style={{ width: `${confidence * 100}%` }}
+                            ></div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                ) : (
+                  <p className="categorie-description">
+                    No pathologies could be ruled out.
+                  </p>
+                )}
+              </section>
+            </div>
+          );
+        })()}
+
+      {/*}
       {result && (
         <div className="analysis-section">
           <h3 className="results-label">Analysis Results</h3>
           <div className="results-container">
             {console.log("Rendering result:", result)}{" "}
-            {/* Check if the result is being passed correctly*/}
+            {/* Check if the result is being passed correctly
             {Object.entries(result)
               .sort(([, valueA], [, valueB]) => valueB - valueA) // Sort in descending order
               .map(([key, value]) => (
@@ -136,6 +299,7 @@ const AnalyzePage = () => {
           </div>
         </div>
       )}
+      */}
     </div>
   );
 };
